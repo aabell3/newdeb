10eea2a9c1f2225f064344580d0bbaf17c0e4572
Barock <aabell3@wgu.edu>
2017-03-08 05:16:34 -0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,32 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
-myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
 
-#info
-echo 
-" INTSALL SQUID3/PROXY ONLY"
-echo 
-" AUTOSCRIPT START 1%"
-
-#update
-apt-get update
-apt-get -y install wget
-clear
-
-echo 
-" 27% COMPLETE "
-
-#installing squid3
-apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.github.com/drcyber96/autoscriptwebmin/master/conf/squid3.conf"
-sed -i $MYIP2 /etc/squid3/squid.conf;
-service squid3 restart
+# go to root
 cd
 
-echo " 58% COMPLETE "
-echo " 91% COMPLETE "
-echo "100% COMPLETE"
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.github.com/yurisshOS/debian7os/master/squid3.conf"
+sed -i $MYIP2 /etc/squid3/squid.conf;
+service squid3 restart
 echo " BYE-BYE"
 
 clear
